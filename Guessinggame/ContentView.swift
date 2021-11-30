fe7ccb6c21a58f30f85f9221a9fb2745c06f4d67
Mateo Elosua <melosua@lcs.on.ca>
2021-11-30 14:26:02 -0500

    Initial Commit

---
+++ Guessinggame/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Guessinggame
+//
+//  Created by Mateo Elosua on 2021-11-30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Guessinggame/GuessinggameApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessinggameApp.swift
+//  Guessinggame
+//
+//  Created by Mateo Elosua on 2021-11-30.
+//
+
+import SwiftUI
+
+@main
+struct GuessinggameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
